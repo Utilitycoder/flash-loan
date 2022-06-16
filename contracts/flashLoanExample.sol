6367d9515742a96990b314dd91810a19eef03f55
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-06-16 08:47:33 +0100

    update

--- contracts/flashLoanExample.sol
+++ contracts/flashLoanExample.sol
@@ -12,7 +12,6 @@ contract FlashLoanExample is FlashLoanSimpleReceiverBase {
     event Log(address asset, uint val);
  
     constructor(IPoolAddressesProvider provider) 
-        public 
         FlashLoanSimpleReceiverBase(provider) 
         
     {}
